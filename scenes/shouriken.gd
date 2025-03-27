2f765c84cfc1ef221fa2aed8f68b221d4bf42929
kotus2 <Kotus322@gmail.com>
2025-03-27 09:38:55 +0200

    not working shouriken 2

--- scenes/shouriken.gd
+++ scenes/shouriken.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 
 var SPEED = 300.0
 
-var left = $/testLevel/player/Ninja.flip_h
+# do the new input shot right , when second button is pressed
+# also do the new input for left we make workaround
+
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
