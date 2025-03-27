ce6177a8b8366611ee4bca3ab02292266c6ff8e0
kotus2 <Kotus322@gmail.com>
2025-03-27 09:23:13 +0200

    not working shourien

--- scenes/shouriken.gd
+++ scenes/shouriken.gd
@@ -1,24 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+var SPEED = 300.0
 
-
+var left = $/testLevel/player/Ninja.flip_h
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	if Input.is_action_just_pressed("shoot"):
+		if left : velocity.x = -300 
+		else: velocity.x = 300
+		
+		move_and_slide()
