49da4bc9de3b3069ea80220f1cf1bc113fa8bcfa
Compsand <uula.salmijarvi@gmail.com>
2025-04-02 23:07:18 +0300

    epic "revamp"
    i did something and added some things and made the shouriken work

--- scenes/shouriken.gd
+++ scenes/shouriken.gd
@@ -5,11 +5,24 @@ var SPEED = 300.0
 
 # do the new input shot right , when second button is pressed
 # also do the new input for left we make workaround
-
+var EVILMODE = 0
 
 func _physics_process(delta: float) -> void:
+	
+	
+	var left = get_node("../player/Ninja").flip_h
+	
+	rotation += 1
+	
 	if Input.is_action_just_pressed("shoot"):
 		if left : velocity.x = -300 
 		else: velocity.x = 300
-		
-		move_and_slide()
+		position = get_node("../player").position
+	
+	move_and_slide()
+	
+	if is_on_wall():
+		EVILMODE += 1
+	elif EVILMODE > 5:
+		position.y = 1000
+		EVILMODE = 0
