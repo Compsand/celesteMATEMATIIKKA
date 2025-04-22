bcda72cb16dfa85c57c91ea4036a2e2b77e74ec1
Compsand <uula.salmijarvi@gmail.com>
2025-04-22 22:43:33 +0300

    MORE POLISH UPDATE
    fixed some fps inconsistencies, also more minor polishing. nothing else. nothing at all. it's just polishing. seriously. really. why would i even lie about something like this. there's no secrets to be had here.

--- projectile.gd
+++ projectile.gd
@@ -3,7 +3,7 @@ extends Area2D
 var lifetime = 600
 
 func _process(delta: float) -> void:
-	position += transform.x * 1
+	position += transform.x * delta * 60
 	
 	if overlaps_area($"../player/stupid"):
 		get_tree().change_scene_to_file("res://scenes/space.tscn")
