712499f0eef36803faf020e87603707f307a6f2d
kotus2 <Kotus322@gmail.com>
2025-09-28 14:09:01 +0300

    Jumping enemy still WIP

--- scenes/jump_enemy.gd
+++ scenes/jump_enemy.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
  
+func _process(_delta: float) -> void: movement()
 
 func movement():
 	velocity.x = -150
@@ -7,10 +8,8 @@ func movement():
 		velocity.x = 150
 	elif is_on_wall() and velocity.x == 150:
 		velocity.x = -150
-
 	if is_on_floor_only():
 		velocity.y == 100
-		get_gravity()
-	
+		
+	get_gravity()
 	move_and_slide()
-	
