550d935b9042bf2c13865fdb775d4e84b3d168df
kotus2 <Kotus322@gmail.com>
2025-09-30 18:14:15 +0300

    jumping enemy in good WIÅP

--- scenes/jump_enemy.gd
+++ scenes/jump_enemy.gd
@@ -3,13 +3,23 @@ extends CharacterBody2D
 func _process(_delta: float) -> void: movement()
 
 func movement():
-	velocity.x = -150
-	if is_on_wall() and velocity.x == -150:
-		velocity.x = 150
-	elif is_on_wall() and velocity.x == 150:
-		velocity.x = -150
+	velocity.x = -50
+	if is_on_wall() and velocity.x == -50:
+		velocity.x = 50
+		print()
+	elif is_on_wall() and velocity.x == 50:
+		velocity.x = -50
 	if is_on_floor_only():
-		velocity.y == 100
+		velocity.y = -50
 		
 	get_gravity()
 	move_and_slide()
+
+
+
+
+func _on_kill_zone_area_entered(area: Area2D) -> void:
+	print("womp")
+	$"../sord".play()
+	queue_free()
+	
